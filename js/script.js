618c46168d069dc9e8f9f1e45d5f18d9bc75abee
MyUsername <my@email.com>
2022-11-18 15:42:02 +0100

    insert last text on js file

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@
 
 let userName = prompt('Ciao, Benvenuto come ti chiami?')
 
-let UserSecondName = prompt('Dimmi il tuo cognome.. e ti dirò chi sei')
+let userSecondName = prompt('Dimmi il tuo cognome.. e ti dirò chi sei')
+
+let favoriteColor = prompt('Qual è il tuo colore preferito?')
+
+let passWord = userName + userSecondName + favoriteColor + 22
+
+document.getElementById("newPassword").innerHTML = passWord
